5454a22c559955ad9c08725603285ac6c446c721
LGBTQIAHistoryHQ <144554864+volson1@users.noreply.github.com>
2025-09-25 11:42:03 -0500

    Initial commit
    Test to show how multithreading could work in current system, that doesn't actually interact with hardware

---
+++ full_front_measurement.py
@@ -0,0 +1,197 @@
+"""from north import NorthC9
+from Locator import *
+
+from powder_settings import *
+from dispensing_helper_functions import *
+from temperature_control_v2 import *
+from elevator_commands import *
+from mold_linear_slide import *
+from pick_and_place_helper_functions import *
+from lepton_recording import LeptonRecorder"""
+import time
+import threading
+from helper_functions import *
+
+"""recorder = LeptonRecorder()
+elevator = MoldElevator(port='COM5')"""
+
+#Variable Initialization
+global N_CHANNELS
+global P_CHANNEL_N
+
+N_MOLDS = 2
+N_CHANNELS = 4
+
+PIPETTE = 0
+LIQUID_REAGENT_1 = 1
+LIQUID_REAGENT_2 = 2
+
+ALIQUOT_SIZE = 0.95  # amount for mold
+AIR_GAP = 0.05       # aliquot + air gap should be < pipette volume
+
+P_CHANNEL_N = 0
+
+gloVars = [P_CHANNEL_N, N_CHANNELS]
+
+# Robot Initialization
+# c9 = NorthC9('sim') # FOR SIMULATION RUN
+
+# * Creating Instance of NorthC9
+"""c9 = NorthC9('A', network_serial = "FT6VDV77")
+c9.default_vel = 25 #  %
+p2 = NorthC9('C', network = c9.network)"""
+
+"""c9.get_info()
+p2.get_info()"""
+
+"""# Testing pumps
+init_system(c9, PIPETTE, LIQUID_REAGENT_1, LIQUID_REAGENT_2)
+# Setting powder cartridge up
+load_powder_cartridge(c9, speed = 1, position = powder_base_left)
+init_powder(p2)
+# Resetting elevator
+elevator.home()"""
+
+def beginToSon(sample_num):
+    # * disarm_heater(2)
+    # elevator.goto_slot(sample_num)
+    print("Positioning elevator")
+    time.sleep(1)
+    
+    # * 1. Get Vial
+    get_vial(sample_num)
+
+    # * 2. Uncap Vial
+    cap_height = uncap_vial()
+    
+    # * 3. Fill Vial
+    # GC2
+    fill_GC2()
+    # Dispense DCPD
+    dispense_DCDP()
+    # TBP
+    #     c9.move_carousel(112.5, 88.5)
+    #     dispense(c9, LIQUID_REAGENT_2, 0.00205)
+    #     c9.move_carousel(202.5, 88.5)
+    #     c9.delay(5)
+    """c9.move_carousel(0, 0)"""
+    
+    # * 4. Recap Vial & Mix in TBP
+    recapMixTBP(cap_height)
+    
+    # * 5. Round 1 Mix & Dry
+    #c9.delay(180) # 3 min
+    if ((sample_num == 0) | (N_MOLDS < 2)):
+        sonicate(180)
+    else:
+        thread1 = threading.Thread(target=channel_and_pipette, args=(2, gloVars))
+        thread1.start()
+
+        sonicate(180)
+    
+        thread1.join()
+
+    dry()
+    
+    # * 6. Round 2 Mix & Dry
+    if ((sample_num == 0) | (N_MOLDS < 2)):
+        sonicate(60)
+        
+        dry()
+
+        place_vial_in_safe()
+    else:
+        thread2 = threading.Thread(target=channel, args=(1, gloVars))
+        thread2.start()
+
+        sonicate(60)
+
+        thread2.join()
+
+        dry()
+
+    #c9.delay(1)
+    #c9.delay(60) # 1 min
+
+    # Reset after sonication
+    """c9.reduce_axis_position(c9.GRIPPER)  # mod gripper position by 4000
+    c9.goto_safe(home)"""
+    
+    # c9.home_axis(c9.GRIPPER, wait = False)
+
+def i_and_d_to_end():
+    # * 9. Intake & Dispense Solution into Channels
+    for channel in range(gloVars[0], N_CHANNELS):
+        if gloVars[0] > 7:
+            raise RuntimeError("Cannot dispense, all channels have already been filled")
+        fill_channel(gloVars[0], N_CHANNELS)
+        gloVars[0] += 1
+    gloVars[0] = 0
+    
+    # Preheat the enviro chamber
+    # ! Will probably need to move up to compenstate for threading time savings
+    """activate_heater(2, 200)"""
+
+    frontT = threading.Thread(target=prompt_front)
+    frontT.start()
+    print("3rd thread opened")
+
+    # * 10. Remove Pipette
+    # c9.goto_safe(p_tip_remover_approach)
+    # c9.goto(p_remover, accel=5)
+    # c9.move_z(292) # move up to max height to dislodge the tip
+    """remove_pipette(c9)"""
+    print("Removing pipette")
+    time.sleep(2)
+    
+    # * 11. Recap Vial
+    recap_vial()
+
+    print("Returning vial to rack")
+    time.sleep(10)
+    """c9.goto_safe(vial_clamp)
+    # * 12. Return Vial to Rack
+    c9.close_gripper()
+    c9.open_clamp()
+    c9.goto_safe(vial_rack_lifted[sample_num])
+    c9.open_gripper()"""
+
+    frontT.join()
+    
+    """unload_mold(c9)"""
+    print("Unloading mold")
+
+
+# * ALL HEATERS RESET, THEN SET ENVIRO CHAMBER TO 30°C
+"""disarm_heater(1)
+disarm_heater(2)
+disarm_heater(3)
+time.sleep(0.2)
+activate_heater(3, 30)"""
+
+#Begin Routine
+for sample_num in range(N_MOLDS):
+    beginToSon(sample_num=sample_num)
+
+    if ((sample_num == 0) & (N_MOLDS > 1)):
+        continue
+    
+    # * 7. Uncap Vial
+    # ! Update to take vial from safe area not directly from sonicator
+    
+    # * 8. Get Pipette
+    # c9.goto_safe(p_rack_side[sample_num])
+    # Parallel code gets pipette and starts dispensing into channels during sonication
+    
+    i_and_d_to_end()
+    
+    if ((sample_num == N_MOLDS - 1) & (N_MOLDS > 1)):
+        place_vial_in_safe()
+        uncap_vial()
+        i_and_d_to_end()
+    
+"""deactivate_heater(3)
+elevator.home()
+unload_powder_cartridge(c9, speed = 1, position = powder_base_left)
+c9.goto_safe(home)"""
+print("End of program")
---
+++ helper_functions.py
@@ -0,0 +1,183 @@
+import time
+
+def home_pump_N2(pump_num, speed_code = 12):
+    """c9.home_pump(pump_num)
+    if not c9.sim:
+        c9.send_com_msg(0, b'/' + str(pump_num+1).encode(encoding='charmap') + b'N2R\x0D')  # set to increment mode N2, must be done every power cycle
+    c9.set_pump_speed(pump_num, speed_code)"""
+
+
+# * Functions I created to simplify main for loop
+def get_vial(sample_num: int):
+    """c9.goto_safe(vial_rack_lifted[sample_num])
+    c9.close_gripper()
+    c9.goto_safe(vial_clamp)
+    c9.close_clamp()"""
+    print("Getting vial") 
+    time.sleep(2)
+
+
+def uncap_vial():
+    """uncap(c9)
+    cap_height = c9.get_axis_position(c9.Z_AXIS)
+    c9.goto_safe(home)  # get out of the way"""
+    
+    print("Uncapping vial and saving the cap height")
+    time.sleep(0.5)
+    return -1
+
+def fill_GC2():
+    """c9.open_clamp()
+    c9.move_carousel(67.5, 75)
+    cl_pow_dispense(c9, p2, 3.2, gc2_ps, False)
+    c9.close_clamp()"""
+
+    print("Filling GC2")
+    #time.sleep(20)
+
+def dispense_DCDP():
+    """c9.move_carousel(0, 0)
+    c9.move_carousel(45, 80)
+    dispense(c9, LIQUID_REAGENT_1, 5)"""
+
+    print("Dispensing DCDP")
+    #time.sleep(10)
+
+def recapMixTBP(cap_height: int):
+    """c9.goto_xy_safe(vial_clamp)
+    c9.move_axis(c9.Z_AXIS, cap_height)
+    cap(c9)
+    c9.open_clamp()
+    c9.move_z(150)
+    #alternating_mix(c9, 5, 80, 4)"""
+
+    print("Recapping and mixing in TBP")
+    #time.sleep(10)
+
+def sonicate(dur: int):
+    # ! Method will likely have to be updated to make sure the arm regrabs the vial at the end of sonication
+    """sonicate(c9, duration = dur)"""
+    print("Beginning sonication")
+    time.sleep(dur)
+    print("Sonication over")
+
+def dry():
+    # ! Method will likely have to be updated to make sure the arm regrabs the vial before drying
+    ''' c9.goto_safe(g_sponge_adjacent)
+    c9.goto(g_sponge)
+    alternating_mix(c9, 5, 5, 4)
+    c9.goto(g_sponge_adjacent)
+    c9.move_z(85)
+    alternating_mix(c9, 5, 40, 4) '''
+    print("Drying")
+    time.sleep(10)
+
+def uncap_vial_post_sonic():
+    """c9.goto_safe(vial_clamp)
+    c9.close_clamp()
+    c9.open_gripper()
+    load_mold(c9)
+    c9.goto_safe(vial_clamp)
+    c9.close_gripper()
+    uncap(c9)"""
+    print("Uncapping vial")
+    time.sleep(0.5)
+
+def recap_vial():
+    """c9.goto_xy_safe(vial_clamp)
+    c9.move_axis(c9.Z_AXIS, cap_height)
+    cap(c9)
+    c9.open_gripper()
+    c9.goto_safe(home)"""
+
+    print("Recapping vial")
+    time.sleep(0.5)
+
+def let_vial_go():
+    time.sleep(5)
+    print("Vial has been let go of")
+
+def fill_channel(P_CHANNEL_N, N_CHANNELS):
+    if P_CHANNEL_N >= N_CHANNELS:
+        raise RuntimeError("Can't dispense, the number of filled channels would exceed N_CHANNELS")
+    if P_CHANNEL_N > 7:
+        raise RuntimeError("Cannot dispense, all channels have already been filled")
+    """c9.goto_safe(p_vial_clamp)
+    c9.aspirate_ml(PIPETTE, ALIQUOT_SIZE)
+    c9.move_z(180)  # should be just out of vial
+    c9.aspirate_ml(PIPETTE, AIR_GAP)  # draw air gap to prevent drips
+    c9.goto_safe(p_mold_loading[P_CHANNEL_N])
+    c9.dispense_ml(PIPETTE, ALIQUOT_SIZE + AIR_GAP)"""
+
+    time.sleep(65)
+    P_CHANNEL_N = P_CHANNEL_N + 1
+    return P_CHANNEL_N
+
+def fill_channels(num, P_CHANNEL_N, N_CHANNELS):
+    for channel in range(num):
+        if P_CHANNEL_N >= N_CHANNELS:
+            print("Can't dispense, the number of filled channels would exceed N_CHANNELS")
+            break
+        if P_CHANNEL_N > 7:
+            raise RuntimeError("Cannot dispense, all channels have already been filled")
+        """c9.goto_safe(p_vial_clamp)
+        c9.aspirate_ml(PIPETTE, ALIQUOT_SIZE)
+        c9.move_z(180)  # should be just out of vial
+        c9.aspirate_ml(PIPETTE, AIR_GAP)  # draw air gap to prevent drips
+        c9.goto_safe(p_mold_loading[P_CHANNEL_N])
+        c9.dispense_ml(PIPETTE, ALIQUOT_SIZE + AIR_GAP)"""
+
+        time.sleep(65)
+        P_CHANNEL_N = P_CHANNEL_N + 1
+        print("Have filled " + str(P_CHANNEL_N) + " channels")
+        
+    return P_CHANNEL_N
+
+def channel_and_pipette(num, gloVars):
+    print("Starting process of filling channels in second thread")
+
+    let_vial_go()
+    # c9.goto_safe(p_rack_side[sample_num])
+    print("Getting pipette")
+
+    uncap_vial()
+
+    P_CHANNEL_N = gloVars[0]
+    N_CHANNELS = gloVars[1]
+
+    i = fill_channels(num, P_CHANNEL_N, N_CHANNELS)
+    gloVars[0] = i
+
+def channel(num, gloVars):
+    print("Starting process of filling channels in second thread")
+
+    let_vial_go()
+    P_CHANNEL_N = gloVars[0]
+    N_CHANNELS = gloVars[1]
+
+    i = fill_channels(num, P_CHANNEL_N, N_CHANNELS)
+    gloVars[0] = i
+    
+    
+def prompt_front():
+    """linear_slide_go_right()
+#     try:
+#         recorder.start() # Try/finally method seems to block, not used
+#         c9.delay(60)
+#     finally:
+#         recorder.stop()
+    
+    recorder.start()
+    c9.delay(80)
+    recorder.stop()
+
+    time.sleep(0.2)
+    disarm_heater(2)
+    linear_slide_go_left()"""
+
+    print("Linear slide going right and waiting for front")
+    time.sleep(80)
+    print("Prompt started linear slide back to the left")
+
+def place_vial_in_safe():
+    print("Placing vial in safe area")
